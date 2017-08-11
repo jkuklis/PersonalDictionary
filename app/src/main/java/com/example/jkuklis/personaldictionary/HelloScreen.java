8a91b22d6a6515d1e6f7a94ad22f3d1492578ced
Jakub Kuklis <j.kuklis@samsung.com>
2017-08-11 09:15:55 +0200

    First

---
+++ app/src/main/java/com/example/jkuklis/personaldictionary/HelloScreen.java
@@ -0,0 +1,15 @@
+package com.example.jkuklis.personaldictionary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_screen);
+    }
+
+
+}
